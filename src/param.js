68366dcc75009ace88f27d5247195e956e20df85
guojun <guojunseven@gmail.com>
2021-12-10 19:16:58 -0800

    try to disable cache 3

--- src/param.js
+++ src/param.js
@@ -2,6 +2,9 @@ const LoggedIn = 1;
 const LoggedOut = 0;
 const Undefined = -1;
 const DefaultAlertState = {type: 'danger', msg: ''};
-const config = {headers: {'Content-Type': 'application/json','Cache-Control' : 'no-cache'}};
+const config = {headers: {
+    'Content-Type': 'application/json','Cache-Control' : 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': 0}};
 
 export default {LoggedIn, LoggedOut, Undefined, DefaultAlertState, config};
